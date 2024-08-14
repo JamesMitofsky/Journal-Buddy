af271cbab3311908b9563456207ff9c105b57684
James Mitofsky <JamesMitofsky@gmail.com>
2024-08-14 17:22:40 +0200

    Add locations map to the settings (pending main feature implementation)

--- components/DefaultSettingsDialog.tsx
+++ components/DefaultSettingsDialog.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription, DialogFooter, DialogHeader,
-    DialogTitle
+  Dialog,
+  DialogContent,
+  DialogDescription, DialogFooter, DialogHeader,
+  DialogTitle
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -12,25 +13,45 @@ import { useDialog } from "@/hooks/useDialog";
 import useLocalStorageState from "use-local-storage-state";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
+import { useState } from "react";
 
 export function DefaultSettingsDialog() {
   const { isDialogOpen, toggleIsDialogOpen } = useDialog();
-  const { toast } = useToast()
+  const { toast } = useToast();
   
-  const [journalNumber, setJournalNumber] = useLocalStorageState<number>('journalNumber')
+  const [journalNumber, setJournalNumber] = useLocalStorageState<number>('journalNumber');
+  const [existingMapLocations, setExistingMapLocations] = useLocalStorageState<Record<string, string>>('mapLocations', {
+    defaultValue: { "Work": "5678" },
+  });
+  const [newLocation, setNewLocation] = useState<string>('');
+  const [newCode, setNewCode] = useState<string>('');
 
   const handleSetJournalNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setJournalNumber(Number(event.target.value))
+    setJournalNumber(Number(event.target.value));
     toast({
         title: 'Autosaved',
         description: 'The new journal number has been autosaved.',
     });
-  }
+  };
 
+  const handleAddLocation = () => {
+    if (newLocation && newCode) {
+      setExistingMapLocations({
+        ...existingMapLocations,
+        [newLocation]: newCode,
+      });
+      setNewLocation('');
+      setNewCode('');
+      toast({
+        title: 'Location Added',
+        description: 'The new Google Plus Code has been added.',
+      });
+    }
+  };
 
   return (
     <Dialog open={isDialogOpen} onOpenChange={toggleIsDialogOpen}>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="max-w-fit">
         <DialogHeader>
           <DialogTitle>Default Settings</DialogTitle>
           <DialogDescription>
@@ -50,11 +71,49 @@ export function DefaultSettingsDialog() {
               className="col-span-3"
             />
           </div>
+          <div className="grid grid-cols-8 items-center gap-4">
+            <Label htmlFor="google-plus-code" className="col-span-2 text-right">
+              Add A Google Plus Code
+            </Label>
+            <Input
+              id="new-location"
+              value={newLocation}
+              onChange={(e) => setNewLocation(e.target.value)}
+              placeholder="Location"
+              type="text"
+              className="col-span-3"
+            />
+            <Input
+              id="new-code"
+              value={newCode}
+              onChange={(e) => setNewCode(e.target.value)}
+              placeholder="Plus Code"
+              type="text"
+              className="col-span-3"
+            />
+            <Button onClick={handleAddLocation} className="col-span-4">Add Location</Button>
+          </div>
         </div>
-        <DialogFooter>
-          <Button type="submit" onClick={toggleIsDialogOpen}>Close Dialog</Button>
+         <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Location Name</TableHead>
+                <TableHead>Google Plus Code</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Object.keys(existingMapLocations).map((location) => (
+                <TableRow key={location}>
+                  <TableCell>{location}</TableCell>
+                  <TableCell>{existingMapLocations[location]}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+          <DialogFooter>
+            <Button type="submit" onClick={toggleIsDialogOpen}>Close Dialog</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
