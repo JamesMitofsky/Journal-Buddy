aac518bd9807117d0bea2747aa8e5634b4a64366
James Mitofsky <JamesMitofsky@gmail.com>
2024-08-17 21:03:28 +0200

    Add fake data randomly to the map

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,3 +1,4 @@
+import React from "react"
 import { MapPin } from "lucide-react"
 import { MapContainer, TileLayer } from "react-leaflet"
 import { Marker, MarkerLayer } from "react-leaflet-marker"
@@ -8,16 +9,30 @@ type LeafletMapProps = {
   geoData?: LatLongFormat[]
 }
 
-const fakeData = [
+const fakeData: LatLongFormat[] = [
   { lat: 37.7749, lng: -122.4194 },
   { lat: 37.7749, lng: -122.4194 },
 ]
+
+function LocationMarkers({ markers }: { markers?: LatLongFormat[] }) {
+  return (
+    <React.Fragment>
+      {markers?.map((marker) => (
+        <MarkerLayer>
+          <Marker position={marker}>
+            <MapPin />
+          </Marker>
+        </MarkerLayer>
+      ))}
+    </React.Fragment>
+  )
+}
 
 export default function LeafletMap({ geoData }: LeafletMapProps) {
   return (
     <MapContainer
       className="size-full"
-      center={fakeData ? fakeData[0] : [0, 0]}
+      center={geoData ? geoData[0] : [0, 0]}
       zoom={13}
       scrollWheelZoom={true}
     >
@@ -25,13 +40,14 @@ export default function LeafletMap({ geoData }: LeafletMapProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {fakeData?.map((pos) => (
+      {/* {fakeData?.map((pos) => (
         <MarkerLayer>
           <Marker position={pos}>
             <MapPin />
           </Marker>
         </MarkerLayer>
-      ))}
+      ))} */}
+      <LocationMarkers markers={geoData} />
     </MapContainer>
   )
 }
--- components/MapOfEntries.tsx
+++ components/MapOfEntries.tsx
@@ -1,10 +1,13 @@
 "use client"
 
-import { useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import dynamic from "next/dynamic"
 
 import { MetadataType } from "@/types/MetadataType"
-import { convertPlusCodeToLatLong } from "@/lib/convertPlusCodeToLatLong"
+import {
+  LatLongFormat,
+  convertPlusCodeToLatLong,
+} from "@/lib/convertPlusCodeToLatLong"
 
 import "leaflet/dist/leaflet.css"
 
@@ -21,9 +24,30 @@ export default function MapOfEntries({ entries }: MapOfEntriesProps) {
     )
   }, [entries])
 
+  // const fakeData: LatLongFormat[] = [
+  //   { lat: 37.7749, lng: -122.4194 },
+  //   { lat: 37.7749, lng: -122.4194 },
+  // ]
+  const [fakeData, setData] = useState<LatLongFormat[]>([
+    { lat: 37.7749, lng: -122.4194 },
+    { lat: 37.7749, lng: -122.4194 },
+  ])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newLat = 37.7749 + Math.random() * 0.1 - 0.05 // Random latitude around the initial value
+      const newLng = -122.4194 + Math.random() * 0.1 - 0.05 // Random longitude around the initial value
+      const newEntry = { lat: newLat, lng: newLng }
+
+      setData((prevData) => [...prevData, newEntry])
+    }, 1000)
+
+    return () => clearInterval(interval) // Cleanup interval on component unmount
+  }, [])
+
   return (
     <div className="h-96 w-full">
-      <LeafletMap geoData={formattedLocations} />
+      <LeafletMap geoData={fakeData} />
     </div>
   )
 }
