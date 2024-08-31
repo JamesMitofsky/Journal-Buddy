b9e8ff2f10d1e75cc6d8eca914e2795c8576b899
James Mitofsky <JamesMitofsky@gmail.com>
2024-08-31 20:53:08 +0200

    Add loading state while importing files

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import { JournalMarkdownType } from "@/types/MetadataType"
 import { getMarkdownData } from "@/lib/getMarkdownData"
 import { parseMarkdown } from "@/lib/parseMarkdown"
+import { Spinner } from "@/components/ui/Spinner"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import {
@@ -23,8 +24,10 @@ export default function FolderPicker() {
     []
   )
   const { toast } = useToast()
+  const [isLoading, setIsLoading] = useState(false)
 
   const handleFolderSelection = async () => {
+    setIsLoading(true)
     try {
       // Request access to the folder
       const directoryHandle = await window.showDirectoryPicker()
@@ -51,6 +54,7 @@ export default function FolderPicker() {
       })
       console.error("Error accessing folder:", error)
     }
+    setIsLoading(false)
   }
 
   return (
@@ -92,7 +96,7 @@ export default function FolderPicker() {
         className="rounded-md bg-blue-500 px-6 py-2 text-white hover:bg-blue-600"
         onClick={handleFolderSelection}
       >
-        Select Folder
+        {isLoading ? <Spinner className="text-white" /> : "Select Folder"}
       </Button>
     </div>
   )
